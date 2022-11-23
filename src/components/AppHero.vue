422b63cea2c83805b815850277b979a07631e0f4
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2022-11-23 15:21:32 +0100

    Aggiunge title e immagini hero con vite&vue

---
+++ src/components/AppHero.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  data() {
+    return {
+      imgOne: "src/assets/vue.svg",
+      imgTwo: "vite.svg"
+    }
+  }
+}
+</script>
+
+<template>
+    <div>
+        <img :src="`${imgOne}`" alt="Logo Vue">
+        <img :src="`${imgTwo}`" alt="Logo Vite">
+    </div>
+</template>
+
+<style scoped>
+
+div{
+    text-align: center;
+}
+
+img{
+    width: 9.375rem;
+    margin: .3125rem .625rem;
+}
+
+</style>
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -2,7 +2,7 @@
 export default {
   data() {
     return {
-      title: 'Il titolo della pagina'
+      title: 'La mia prima app con Vite!'
     }
   }
 }
@@ -12,6 +12,11 @@ export default {
   <h1>{{ title }}</h1>
 </template>
 
-<style scoped></style>
+<style scoped>
 
+h1{
+    text-align: center;
+    margin-top: 15.625rem;
+}
 
+</style>
